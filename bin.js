845be057be60383a877726caf9696ef16901de99
Bryan English <bryan@bryanenglish.com>
2017-05-30 23:37:05 -0700

    cli multi args

--- bin.js
+++ bin.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 const covfefe = require('./index')
-const preamble = process.argv[2] || '...'
+const preamble = process.argv[2] ? process.argv.slice(2).join(' ') : '...'
 console.log(covfefe(preamble))
