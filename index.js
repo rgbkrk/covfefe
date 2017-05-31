034ee7b5224313171ee25b45f2753d700613c857
Mohsen Azimi <mazimi@lyft.com>
2017-05-31 09:33:08 -0700

    Add RTL support

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const stringDirection = require('string-direction');
+/**
+ * @param {string} input
+ * @return {string}
+ */
 module.exports = function(input) {
+  if (stringDirection.getDirection(input) === 'rtl') {
+    return `covfefe ${input}`;
+  }
   return `${input} covfefe`;
 };
